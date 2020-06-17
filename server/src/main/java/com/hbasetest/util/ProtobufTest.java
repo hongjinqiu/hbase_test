a6b24ffab1bcc94815c473bf3d5d166d83e68492
jqhong <jqhong@onlyou.com>
2020-06-17 16:22:42 +0800

    添加 google protobuf 协议用于前后端通讯

---
+++ server/src/main/java/com/hbasetest/util/ProtobufTest.java
@@ -0,0 +1,27 @@
+package com.hbasetest.util;
+
+import com.jihite.PersonModel;
+
+public class ProtobufTest {
+    public static void main(String[] args) throws Exception {
+        PersonModel.Person.Builder builder = PersonModel.Person.newBuilder();
+        builder.setId(1);
+        builder.setName("jihite");
+        builder.setEmail("jihite@jihite.com");
+
+        PersonModel.Person person = builder.build();
+        System.out.println("before:" + person);
+
+        System.out.println("===Person Byte:");
+        for (byte b: person.toByteArray()) {
+            System.out.println(b);
+        }
+        System.out.println("===================");
+
+        byte[] byteArray = person.toByteArray();
+        PersonModel.Person p2 = PersonModel.Person.parseFrom(byteArray);
+        System.out.println("after id:" + p2.getId());
+        System.out.println("after name:" + p2.getName());
+        System.out.println("after email:" + p2.getEmail());
+    }
+}
